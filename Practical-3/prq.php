883a8fd367f963368d81569902a61341720aeba4
Dhanshri246 <technerd1124@gmail.com>
2025-01-16 05:22:35 +0000

    rest all  pogrmsPR 2-3

---
+++ Practical-3/prq.php
@@ -0,0 +1,21 @@
+
+    <?php
+
+     for ($i = 0; $i <30; $i++) {
+            if( $i %2== 0) {
+                echo $i."Is Even Number <br>";
+            }
+     }
+
+     while ( $i < 30) {
+        if( $i %2== 0) {
+            echo $i."Is Even Number "."<br>";
+        }
+    }
+
+    do {
+        echo $i." Is Even Number <br<>";
+    } while ( $i < 30);
+    ?>
+
+    
--- welcome_php.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<?php
-echo(“<h1><b>Welcome to PHP</b></h1> ”);
-?>
-  
-</body>
-</html>
